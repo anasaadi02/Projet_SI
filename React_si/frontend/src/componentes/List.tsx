f8907f406c86c6777e4093e3f67fe2b3363296a1
anas <anas2002saadi@gmail.com>
2024-01-04 23:59:59 +0000

    list page added

---
+++ React_si/frontend/src/componentes/List.tsx
@@ -0,0 +1,56 @@
+function List() {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Promo</th>
+          <th scope="col">N°Etudiant</th>
+          <th scope="col">Professeur</th>
+          <th scope="col">Tuteur</th>
+          <th scope="col">Entreprise</th>
+          <th scope="col">Type</th>
+          <th scope="col">Année</th>
+          <th scope="col">Compte rendu</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>2020-2023</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>2020-2023</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>2020-2023</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+          <td>...</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default List;
